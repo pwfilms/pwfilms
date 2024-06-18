8b4cfcadd0184239fd8de176a559b0b586a6209a
pwfilms <paulwebsterdirector@gmail.com>
2024-06-18 19:16:23 +0100

    18June24 JS

---
+++ js/fadein.js
@@ -0,0 +1,32 @@
+$(document).on("scroll", function () {
+    var pageTop = $(document).scrollTop()
+    var pageBottom = pageTop + $(window).height()
+    var tags = $("section")
+    
+    for (var i = 0; i < tags.length; i++) {
+    var tag = tags[i]
+    
+    if ($(tag).position().top < pageBottom) {
+    $(tag).addClass("visible")
+    } else {  $(tag).removeClass("visible")
+    }
+    }
+})
+
+$(document).on("scroll", function () {
+    var pageTop = $(document).scrollTop()
+    var pageBottom = pageTop + $(window).height()
+    var tags = $(".fadeUp, .fadeUpDelayed, .fadeLeft, .fadeRight, .fadeIn")
+    
+    for (var i = 0; i < tags.length; i++) {
+    var tag = tags[i]
+    
+    if ($(tag).position().top < pageBottom) {
+    $(tag).addClass("visible")
+    } else {  $(tag).removeClass("visible")
+    }
+    }
+})
+
+
+
